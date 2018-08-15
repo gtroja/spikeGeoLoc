1a513817559b0b52d5358e3a75b32c2f9d09203f
Guilherme Trojan <guilhermetrojan@alunos.utfpr.edu.br>
2018-08-15 10:14:40 -0300

    implementado geoloc

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  message : string;
+
+  print( text ){
+    this.message += text + '\n'
+  }
+
+  constructor(
+    public navCtrl: NavController,
+    private geolocation: Geolocation
+  ){
+    this.message = "ola mundo\n"
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.print("lat "+resp.coords.latitude)
+      this.print("lon "+resp.coords.longitude)
+      
+     }).catch((error) => {
+       this.print('erro :(' + JSON.stringify(error))
+     });
+     
+     let watch = this.geolocation.watchPosition();
+     watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+      this.print("STREAM: lat: " + data.coords.latitude)
+      this.print("STREAM: long: " + data.coords.longitude)
+     });
+
 
   }
 
